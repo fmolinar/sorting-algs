c433f8dc90cd06f4e2e3a7c219189e46a49057d7
fmr999 <fer.molina.rdz@gmail.com>
2018-01-20 16:30:59 -0800

    Update bubbleSort.cpp

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -25,24 +25,27 @@ int main()
 void bubbleSort(int arr[], int sizeOfArray)
 {
 /* Bubble sort works as follows: adjacent elements are compared and
- * swapped*/
+ * swapped until the last one*/
 	int iterationNumber = 1;
 	// list will be use to traverse the entire array starting from
 	// index 2 to 
-	for(int list = 1; list < sizeOfArray; list++)
+	for(int list = sizeOfArray - 1; list >= 0; list--)
 	{
 		// loop that will iterate backwards checking if the order of the 
 		// array matches, it will be swapping the values until the array 
 		// its in order then it will exit
-		for(int check = list; check > 0 && arr[check] < arr[check-1]; check--)
+		for(int check = 1; check <= list ; check++)
 		{
+			if(arr[check] < arr[check-1])
+			{
+				int temp = arr[check];
+				arr[check] = arr[check - 1];
+				arr[check - 1] = temp;
+			}
 			printArray(arr, sizeOfArray);
-			cout<<"Iteration number: "<<iterationNumber<<endl;
-			// swap the values
-			int temp = arr[check];
-			arr[check] = arr[check - 1];
-			arr[check - 1] = temp;
 		}
+		cout<<"Iteration number: "<<iterationNumber<<endl;
+
 		iterationNumber++;
 		
 	}
